4cae62eaca9c0ea9015143fb6ab7b6eb1a50c9a1
kimhyeji <gpwl5589@gamil.com>
2025-07-31 16:42:29 +0900

    02-12, answer create

--- src/main/java/com/mysite/sbb/answer/AnswerController.java
+++ src/main/java/com/mysite/sbb/answer/AnswerController.java
@@ -16,11 +16,14 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class AnswerController {
     private final QuestionService questionService;
+    private final AnswerService answerService;
 
     @PostMapping("/create/{id}")
     public String createAnswer(Model model, @PathVariable("id") Integer id, @RequestParam(value="content") String content) {
         Question question = this.questionService.getQuestion(id);
-        // TODO: 답변 등록 구현
-        return "";
+
+        this.answerService.create(question, content);
+
+        return String.format("redirect:/question/detail/%s", id);
     }
 }
