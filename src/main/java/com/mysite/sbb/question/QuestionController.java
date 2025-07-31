17988e4299f701545e7284dd2ddfacad39504f4e
kimhyeji <gpwl5589@gamil.com>
2025-07-31 15:34:23 +0900

    02-11, prefix

--- src/main/java/com/mysite/sbb/question/QuestionController.java
+++ src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -5,16 +5,17 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
-
+@RequestMapping("/question")
 @RequiredArgsConstructor
 @Controller
 public class QuestionController {
     private final QuestionService questionService;
 
-    @GetMapping("/question/list")
+    @GetMapping("/list")
     public String list(Model model) {
         List<Question> questionList = this.questionService.getList();
 
@@ -23,7 +24,7 @@ public class QuestionController {
         return "question_list";
     }
 
-    @GetMapping("question/detail/{id}")
+    @GetMapping(value = "/detail/{id}")
     public String getQuestion(Model model, @PathVariable("id") Integer id) {
         Question question = this.questionService.getQuestion(id);
         model.addAttribute("question", question);
