da397b5a3dc0eddd9446904a7321b2ccb873fedf
kimhyeji <gpwl5589@gamil.com>
2025-08-08 17:35:54 +0900

    03-09, delete part 1

--- src/main/java/com/mysite/sbb/question/QuestionController.java
+++ src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -59,6 +59,7 @@ public class QuestionController {
         SiteUser siteUser = this.userService.getUser(principal.getName());
 
         this.questionService.create(questionForm.getSubject(), questionForm.getContent(), siteUser);
+
         return "redirect:/question/list";
     }
 
@@ -73,8 +74,6 @@ public class QuestionController {
 
         questionForm.setSubject(question.getSubject());
         questionForm.setContent(question.getContent());
-        
-        // TODO: 수정 기능 구현
 
         return "question_form";
     }
